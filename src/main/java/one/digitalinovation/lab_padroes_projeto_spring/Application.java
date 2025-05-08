9fdb39526ac0de8f2992370533af51e5760da31b
Rodrigo Souza <ro6rigo@gmail.com>
2025-05-07 23:35:23 -0300

    commit inicial gerado via Spring Initializr

---
+++ src/main/java/one/digitalinovation/lab_padroes_projeto_spring/Application.java
@@ -0,0 +1,23 @@
+package one.digitalinovation.lab_padroes_projeto_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Projeto Sprint Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author ro6rigo
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
